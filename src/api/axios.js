3c867fd18629cd035440cfa2b4902e8d52d537d6
mianmuhammadbilal <xtylishweb@gmail.com>
2026-07-31 17:14:59 +0500

    Fix Render API URL

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://linkvault-backend-y3h6.onrender.com',
+  baseURL: 'https://linkvault-backend-y3h6.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
